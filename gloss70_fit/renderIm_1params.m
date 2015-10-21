07c80d1520a52bfca105ac394824a90ce3d1e2d4
gzmk <gizemkucukoglu@gmail.com>
2015-10-21 12:16:13 -0400

    corrected mappings filename

--- gloss70_fit/renderIm_1params.m
+++ gloss70_fit/renderIm_1params.m
@@ -70,7 +70,7 @@ parentSceneFile = 'test_sphere.dae';
 % WriteDefaultMappingsFile(parentSceneFile); % After this step you need to edit the mappings file
 
 conditionsFile = 'sphere_3params_Conditions.txt';
-mappingsFile = 'sphere_3params_DefaultMappings.txt';
+mappingsFile = '70gloss_DefaultMappings.txt';
 
 % Make sure all illuminants are added to the path. 
 addpath(genpath(pwd))
